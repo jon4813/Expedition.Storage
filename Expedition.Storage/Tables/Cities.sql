f36a1946112abcf64750dfd6a47580aad45184a7
evgeny.uskov <evgeny.uskov@ligastavok.ru>
2018-11-13 10:47:59 +0300

    Изменил мапинг регионов.

--- Expedition.Storage/Tables/Cities.sql
+++ Expedition.Storage/Tables/Cities.sql
@@ -3,5 +3,14 @@
 	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
     [Name] NVARCHAR(50) NOT NULL,
     [Code] NVARCHAR(50) NOT NULL,
+	[CountryId] UNIQUEIDENTIFIER NOT NULL,
+    [RegionId] UNIQUEIDENTIFIER NULL,
     [RowVersion] ROWVERSION NOT NULL,
+    [IsArchived] BIT NOT NULL DEFAULT 0,
+
+	CONSTRAINT [FK_Cities_Countries] FOREIGN KEY ([CountryId]) REFERENCES [Countries]([Id]),
+	CONSTRAINT [FK_Cities_Regions] FOREIGN KEY ([RegionId]) REFERENCES [Regions]([Id]),
 )
+
+GO
+CREATE INDEX [UX_Cities_CountryId_RegionId_Id] ON [dbo].[Cities] ([CountryId], [RegionId], [Id])
--- Expedition.Storage/Tables/OrderPaths.sql
+++ Expedition.Storage/Tables/OrderPaths.sql
@@ -6,6 +6,6 @@
     [FromCityAddress] NVARCHAR(100) NOT NULL,
     [ToCityPoint] UNIQUEIDENTIFIER NOT NULL,
     [ToCityAddress] NVARCHAR(100) NOT NULL,
-	CONSTRAINT [FK_OrderPaths_RegionsMappings_From] FOREIGN KEY ([FromCityPoint]) REFERENCES [RegionsMappings]([Id]),
-	CONSTRAINT [FK_OrderPaths_RegionsMappings_To] FOREIGN KEY ([ToCityPoint]) REFERENCES [RegionsMappings]([Id])
+	CONSTRAINT [FK_OrderPaths_City_From] FOREIGN KEY ([FromCityPoint]) REFERENCES [dbo].[Cities]([Id]),
+	CONSTRAINT [FK_OrderPaths_City_To] FOREIGN KEY ([ToCityPoint]) REFERENCES [dbo].[Cities]([Id])
 )
